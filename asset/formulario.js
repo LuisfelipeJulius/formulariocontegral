4e824bd7bbf090d355e371b77cef78cb75359340
Luis Felipe <felipe.ibanez@julius2grow.com>
2024-05-08 21:14:40 +0000

    se crearon los archivos

--- asset/formulario.js
+++ asset/formulario.js
@@ -1,69 +1,69 @@
 document.getElementById("miFormulario").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe normalmente
-    
-    // Muestra el modal con el spinner
-    var spinnerModal = new bootstrap.Modal(document.getElementById('spinnerModal'), {
-      backdrop: 'static',
-      keyboard: false
-    });
-    spinnerModal.show();
-    
-    // Captura los datos del formulario
-    var nombre = document.getElementById("nombre").value;
-    var correo = document.getElementById("correo").value;
-    var cc_nit = document.getElementById("cc_nit").value;
-    var municipio = document.getElementById("cliente").value;
-    var apellido = document.getElementById("apellido").value;
-    var celular = document.getElementById("celular").value;
-    var departamento = document.getElementById("ciudad").value;
-    var aceptacionTerminos = document.getElementById("agreeCheckbox").checked;
-    
-    // Construye el objeto de datos
-    var datos = {
-      nombre: nombre,
-      correo: correo,
-      cc_nit: cc_nit,
-      municipio: municipio,
-      apellido: apellido,
-      celular: celular,
-      departamento: departamento,
-      aceptacionTerminos: aceptacionTerminos
-    };
-    
-    // Envía los datos al webhook
-    fetch('https://hook.us1.make.com/xuwt7cdemj1oo3w8vj7irmslasx89hqy', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(datos)
-    })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      // Oculta el modal
-      spinnerModal.hide();
-      // Redirige a la siguiente URL
-      window.location.href = 'https://www.finca.co/';
-    })
-    .catch(function(error) {
-      // Oculta el modal
-      spinnerModal.hide();
-      // Maneja el error, podrías mostrar un mensaje de error o hacer algo más
-      console.error('Hubo un problema con la petición Fetch:', error);
-    });
+  event.preventDefault(); // Evita que el formulario se envíe normalmente
+  
+  // Muestra el modal con el spinner
+  var spinnerModal = new bootstrap.Modal(document.getElementById('spinnerModal'), {
+    backdrop: 'static',
+    keyboard: false
   });
-
+  spinnerModal.show();
+  
+  // Captura los datos del formulario
+  var nombre = document.getElementById("nombre").value;
+  var correo = document.getElementById("correo").value;
+  var cc_nit = document.getElementById("cc_nit").value;
+  var municipio = document.getElementById("cliente").value;
+  var apellido = document.getElementById("apellido").value;
+  var celular = document.getElementById("celular").value;
+  var departamento = document.getElementById("ciudad").value;
+  var aceptacionTerminos = document.getElementById("agreeCheckbox").checked;
+  
+  // Obtiene la fecha actual
+  var fechaRegistro = new Date().toLocaleDateString(); // Obtiene solo la fecha
+  
+  // Construye el objeto de datos
+  var datos = {
+    nombre: nombre,
+    correo: correo,
+    cc_nit: cc_nit,
+    municipio: municipio,
+    apellido: apellido,
+    celular: celular,
+    departamento: departamento,
+    aceptacionTerminos: aceptacionTerminos,
+    fechaRegistro: fechaRegistro // Agrega la fecha de registro
+  };
+  
+  // Envía los datos al webhook
+  fetch('https://hook.us1.make.com/xuwt7cdemj1oo3w8vj7irmslasx89hqy', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(datos)
+  })
+  .then(function() {
+    // Oculta el modal
+    spinnerModal.hide();
+    // Redirige a la siguiente URL
+    window.location.href = 'https://www.finca.co/jornada-ganadera-mayo-2024/registro-exitoso';
+  })
+  .catch(function(error) {
+    // Oculta el modal
+    spinnerModal.hide();
+    // Maneja el error, podrías mostrar un mensaje de error o hacer algo más
+    console.error('Hubo un problema con la petición Fetch:', error);
+  });
+});
 
 
   // popup  tratamiento de datos
 
   document.addEventListener("DOMContentLoaded", function() {
     // Función para mostrar el popup al hacer clic en el botón "Aceptar términos"
-    document.getElementById("popupTrigger").addEventListener("click", function() {
+    /*document.getElementById("popupTrigger").addEventListener("click", function() {
       document.getElementById("popup").style.display = "block";
-    });
+    });*/
   
     // Función para cerrar el popup y marcar el checkbox al hacer clic en el botón "Cerrar"
     document.getElementById("closePopupButton").addEventListener("click", function() {
@@ -93,9 +93,3 @@ document.getElementById("miFormulario").addEventListener("submit", function(even
       // Aquí puedes agregar la lógica para enviar los datos del formulario
     });
   });
-  
-  
-
- 
-
- 
